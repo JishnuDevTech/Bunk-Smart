9f3327b2a0412481a801b4a4a0df5bdb01132a51
Jishnu Rahegaonkar <jishnurahegaonkar@gmail.com>
2026-01-06 09:54:18 +0530

    Delete bunk-smart directory

--- bunk-smart/js/challenges.js
+++
@@ -1,4 +0,0 @@
-// challenges.js - Deprecated
-// Full challenges logic has been migrated to dashboard.js with Firebase integration.
-// This file is no longer used; challenges.html redirects to dashboard.html#challenges.
-console.log('Challenges page deprecated. Use dashboard.');
--- bunk-smart/js/insights.js
+++
@@ -1,4 +0,0 @@
-// insights.js - Deprecated
-// Full insights logic has been migrated to dashboard.js with Firebase integration.
-// This file is no longer used; insights.html redirects to dashboard.html#insights.
-console.log('Insights page deprecated. Use dashboard.');
